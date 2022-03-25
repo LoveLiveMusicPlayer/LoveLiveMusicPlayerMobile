ab381bef33d67bccf0b7be82a83b597ba0a40f39
gr.zhu <55681140@163.com>
2022-03-25 18:01:36 +0800

    merge

--- lib/pages/main/view.dart
+++ lib/pages/main/view.dart
@@ -35,17 +35,20 @@ class _MainPageState extends State<MainPage>
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color(0xFFF2F8FF),
-      // appBar: AppBar(
-      //   toolbarHeight: 54.w,
-      //   elevation: 0,
-      //   backgroundColor: const Color(0xFFF2F8FF),
-      //   title: _getTabBar(),
-      //   actions: [_getTopHead()],
-      // ),
-      // body: _getTabBarView(),
-      body: _buildWeSlide(context),
+    return Container(
+      color: const Color(0xFFF2F8FF),
+      child: Column(
+        children: [
+          AppBar(
+            toolbarHeight: 54.w,
+            elevation: 0,
+            backgroundColor: const Color(0xFFF2F8FF),
+            title: _getTabBar(),
+            actions: [_getTopHead()],
+          ),
+          Expanded(child: _buildWeSlide(context)),
+        ],
+      ),
     );
   }
 
