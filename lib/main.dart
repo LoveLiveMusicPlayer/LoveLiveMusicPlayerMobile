3792effb373c462047485a90cd394fc30e248ab6
zhushenwudi <55681140@163.com>
2023-07-19 06:07:23 +0800

    暂存

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,6 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
-import 'package:flutter_phoenix/flutter_phoenix.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_smart_dialog/flutter_smart_dialog.dart';
 import 'package:get/get.dart';
@@ -103,14 +102,14 @@ void main() async {
     };
 
     runZonedGuarded(
-      () => runApp(Phoenix(child: const MyApp())),
+      () => runApp(const MyApp()),
       (error, stackTrace) {
         // 没被catch的异常
         reportErrorAndLog(makeDetails(error, stackTrace));
       },
     );
   } else {
-    runApp(Phoenix(child: const MyApp()));
+    runApp(const MyApp());
   }
 
   AppUtils.setStatusBar(isDark);
